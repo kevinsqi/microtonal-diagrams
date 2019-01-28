2d08aa4b9ed4f4f2094f6003cf8920cc809f6f81
Kevin Qi <kqi@amino.com>
2019-01-27 16:43:14 -0800

    render svg circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>Microtonal Diagrams</h1>
+
+        <svg viewBox="0 0 100 100">
+          <circle cx="50" cy="50" r="50" />
+        </svg>
       </div>
     );
   }
