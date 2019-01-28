a1e3a32b4952a28d1ff04f63827d3342824cbe03
Kevin Qi <kqi@amino.com>
2019-01-27 23:38:19 -0800

    WIP using forwardRef

--- src/App.tsx
+++ src/App.tsx
@@ -3,63 +3,68 @@ import { saveAs } from "file-saver";
 import range from "lodash.range";
 import "./App.css";
 
-function TuningDiagram(props: {
-  count: number;
-  color: string;
-  radius: number;
-  separation: number;
-}) {
-  const { color, radius, separation } = props;
-  const diameter = radius * 2;
-  const height = diameter;
-  const width = props.count * (diameter + separation) - separation;
-  return (
-    <div>
-      <svg
-        id="diagram"
-        viewBox={`0 0 ${width} ${height}`}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {range(props.count).map(idx => {
-          return (
-            <circle
-              cx={(diameter + separation) * idx + radius}
-              cy={radius}
-              r={radius}
-              style={{ fill: color }}
-            />
-          );
-        })}
-        {range(props.count - 1).map(idx => {
-          const x1 = (diameter + separation) * idx + radius;
-          return (
-            <line
-              x1={x1}
-              y1={radius}
-              x2={x1 + diameter + separation}
-              y2={radius}
-              style={{
-                stroke: color,
-                strokeWidth: 1
-              }}
-            />
-          );
-        })}
-      </svg>
+const TuningDiagram = React.forwardRef(
+  (
+    props: {
+      count: number;
+      color: string;
+      radius: number;
+      separation: number;
+    },
+    ref
+  ) => {
+    const { color, radius, separation } = props;
+    const diameter = radius * 2;
+    const height = diameter;
+    const width = props.count * (diameter + separation) - separation;
+    return (
+      <div>
+        <svg
+          ref={ref}
+          viewBox={`0 0 ${width} ${height}`}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {range(props.count).map(idx => {
+            return (
+              <circle
+                cx={(diameter + separation) * idx + radius}
+                cy={radius}
+                r={radius}
+                style={{ fill: color }}
+              />
+            );
+          })}
+          {range(props.count - 1).map(idx => {
+            const x1 = (diameter + separation) * idx + radius;
+            return (
+              <line
+                x1={x1}
+                y1={radius}
+                x2={x1 + diameter + separation}
+                y2={radius}
+                style={{
+                  stroke: color,
+                  strokeWidth: 1
+                }}
+              />
+            );
+          })}
+        </svg>
 
-      <button
-        className="btn btn-secondary"
-        onClick={() => {
-          const svg: HTMLElement = document.getElementById("diagram")!;
-          const blob = new Blob([svg.outerHTML], { type: "image/svg+xml" });
-          saveAs(blob, "diagram.svg");
-        }}
-      >
-        Download
-      </button>
-    </div>
-  );
-}
+        <button
+          className="btn btn-secondary"
+          onClick={() => {
+            const svg: HTMLElement = document.getElementById("diagram")!;
+            const blob = new Blob([svg.outerHTML], { type: "image/svg+xml" });
+            saveAs(blob, "diagram.svg");
+          }}
+        >
+          Download
+        </button>
+      </div>
+    );
+  }
+);
 
 class App extends Component {
   render() {
