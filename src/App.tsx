fc4bcc47b82ce1f6ce943dc539ac969bc1dd7582
Kevin Qi <kqi@amino.com>
2019-07-28 14:20:48 -0700

    use hook state

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 import ScaleDiagram from "./ScaleDiagram";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Microtone Scale Diagrams</h1>
-        <div style={{ background: "#555" }}>
-          <ScaleDiagram count={12} color="white" radius={4} separation={20} />
-          <ScaleDiagram count={24} color="white" radius={5} separation={10} />
-        </div>
-      </div>
-    );
-  }
-}
+// TODO: rename project to be more general, not specific to microtones
+// TODO: move buttons out of ScaleDiagram
 
-export default App;
+export default function App() {
+  // TODO: be able to specify text for each dot (put input above them?)
+  const [scaleConfig, setScaleConfig] = React.useState({
+    count: 12,
+    color: "white",
+    radius: 5,
+    separation: 20
+  });
+
+  return (
+    <div className="container my-5">
+      <h1 className="text-center">Microtone Scale Diagrams</h1>
+      <div className="mt-5">
+        <ScaleDiagram {...scaleConfig} />
+        <ScaleDiagram count={24} color="black" radius={5} separation={10} />
+      </div>
+    </div>
+  );
+}
