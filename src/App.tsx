3b9139012fb3823a7946251a5c43344514c786d6
Kevin Qi <kqi@amino.com>
2019-07-28 14:03:59 -0700

    extract TuningDiagram.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,8 @@
 import React, { Component } from "react";
 import { saveAs } from "file-saver";
 import { saveSvgAsPng } from "save-svg-as-png";
-import range from "lodash.range";
 import "./App.css";
-
-function TuningDiagram(props: {
-  count: number;
-  color: string;
-  radius: number;
-  separation: number;
-}) {
-  const { color, radius, separation } = props;
-  const diameter = radius * 2;
-  const height = diameter;
-  const width = props.count * (diameter + separation) - separation;
-  return (
-    <div>
-      <svg
-        id="diagram"
-        viewBox={`0 0 ${width} ${height}`}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        {range(props.count).map(idx => {
-          return (
-            <circle
-              cx={(diameter + separation) * idx + radius}
-              cy={radius}
-              r={radius}
-              style={{ fill: color }}
-            />
-          );
-        })}
-        {range(props.count - 1).map(idx => {
-          const x1 = (diameter + separation) * idx + radius;
-          return (
-            <line
-              x1={x1}
-              y1={radius}
-              x2={x1 + diameter + separation}
-              y2={radius}
-              style={{
-                stroke: color,
-                strokeWidth: 1
-              }}
-            />
-          );
-        })}
-      </svg>
-
-      <button
-        className="btn btn-secondary"
-        onClick={() => {
-          const svgElement: HTMLElement = document.getElementById("diagram")!;
-          const svgBlob = new Blob([svgElement.outerHTML], {
-            type: "image/svg+xml"
-          });
-          saveAs(svgBlob, "diagram.svg");
-        }}
-      >
-        Download .svg
-      </button>
-      <button
-        className="btn btn-secondary"
-        onClick={() => {
-          const svgElement: HTMLElement = document.getElementById("diagram")!;
-          saveSvgAsPng(svgElement, "diagram.png", { scale: 4 });
-        }}
-      >
-        Download .png
-      </button>
-    </div>
-  );
-}
+import TuningDiagram from "./TuningDiagram";
 
 class App extends Component {
   render() {
