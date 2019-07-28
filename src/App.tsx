43a1af62722714788af7e632732f0886a106b72e
Kevin Qi <kqi@amino.com>
2019-07-28 14:43:13 -0700

    use grid to lay out inputs

--- src/App.tsx
+++ src/App.tsx
@@ -19,57 +19,67 @@ export default function App() {
       <h1 className="text-center">Microtone Scale Diagrams</h1>
       <div className="mt-5">
         <form>
-          <label>Count</label>
-          <input
-            className="form-control"
-            type="number"
-            value={scaleConfig.count}
-            min={1}
-            max={1000}
-            onChange={event => {
-              setScaleConfig({
-                ...scaleConfig,
-                count: parseInt(event.target.value)
-              });
-            }}
-          />
-          <label>Radius</label>
-          <input
-            className="form-control"
-            type="number"
-            value={scaleConfig.radius}
-            min={1}
-            max={1000}
-            onChange={event => {
-              setScaleConfig({
-                ...scaleConfig,
-                radius: parseInt(event.target.value)
-              });
-            }}
-          />
-          <label>Separation</label>
-          <input
-            className="form-control"
-            type="number"
-            value={scaleConfig.separation}
-            min={1}
-            max={1000}
-            onChange={event => {
-              setScaleConfig({
-                ...scaleConfig,
-                separation: parseInt(event.target.value)
-              });
-            }}
-          />
-          <label>Color</label>
-          <input
-            className="form-control"
-            type="text"
-            value={scaleConfig.color}
-            onChange={event => {
-              setScaleConfig({ ...scaleConfig, color: event.target.value });
-            }}
-          />
+          <div className="row">
+            <div className="col-12 col-md-3">
+              <label>Count</label>
+              <input
+                className="form-control"
+                type="number"
+                value={scaleConfig.count}
+                min={1}
+                max={1000}
+                onChange={event => {
+                  setScaleConfig({
+                    ...scaleConfig,
+                    count: parseInt(event.target.value)
+                  });
+                }}
+              />
+            </div>
+            <div className="col-12 col-md-3">
+              <label>Radius</label>
+              <input
+                className="form-control"
+                type="number"
+                value={scaleConfig.radius}
+                min={1}
+                max={1000}
+                onChange={event => {
+                  setScaleConfig({
+                    ...scaleConfig,
+                    radius: parseInt(event.target.value)
+                  });
+                }}
+              />
+            </div>
+            <div className="col-12 col-md-3">
+              <label>Separation</label>
+              <input
+                className="form-control"
+                type="number"
+                value={scaleConfig.separation}
+                min={1}
+                max={1000}
+                onChange={event => {
+                  setScaleConfig({
+                    ...scaleConfig,
+                    separation: parseInt(event.target.value)
+                  });
+                }}
+              />
+            </div>
+            <div className="col-12 col-md-3">
+              <label>Color</label>
+              <input
+                className="form-control"
+                type="text"
+                value={scaleConfig.color}
+                onChange={event => {
+                  setScaleConfig({ ...scaleConfig, color: event.target.value });
+                }}
+              />
+            </div>
+          </div>
         </form>
       </div>
       <div className="mt-5">
